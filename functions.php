5075608e030d7c8a638d2716a3238eacc5566fbf
ailequal <37016865+ailequal@users.noreply.github.com>
2020-03-09 17:43:11 +0100

    Added get_by_id()

--- functions.php
+++ functions.php
@@ -8,8 +8,8 @@
 		if ($result && $result->num_rows > 0) {
 			// store every row inside the array
 			$rooms = [];
-		while($row = $result->fetch_assoc()) {
-			$rooms[] = $row;
+			while($row = $result->fetch_assoc()) {
+				$rooms[] = $row;
 			}
 		} elseif ($result) {
 			echo "No results";
@@ -18,5 +18,21 @@
 		}
 		$conn->close();
 		return $rooms;
+	}
+
+	function get_by_id($conn, $table, $id) {
+		// sql query selecting the row with specific id in the table
+		$sql = "SELECT * FROM $table WHERE `id` =  $id";
+		$result = $conn->query($sql);
+
+		if ($result && $result->num_rows > 0) {
+			$room = $result->fetch_assoc();
+		} elseif ($result) {
+			echo "No results";
+		} else {
+			echo "Query error";
+		}
+		$conn->close();
+		return $room;
 	}
 ?>
--- show/server.php
+++ show/server.php
@@ -1,5 +1,5 @@
 <?php
-
+	include __DIR__ . '/../functions.php';
 	include __DIR__ . '/../database.php';
 
 	// get room id
@@ -7,18 +7,6 @@
 		die('No ID selected');
 	}
 	$id_room = $_GET['id'];
-
-	// sql query
-	$sql = "SELECT `id`, `room_number`, `floor`, `beds` FROM `stanze` WHERE `id` =  $id_room";
-	$result = $conn->query($sql);
-
-	if ($result && $result->num_rows > 0) {
-		$room = $result->fetch_assoc();
-	} elseif ($result) {
-		echo "No results";
-	} else {
-		echo "Query error";
-	}
-	$conn->close();
-
+	
+	$room = get_by_id($conn, 'stanze', $id_room);
 ?>
